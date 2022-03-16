315d73a4bf530ffeeac857691e0879945387526f
Abhijeet43 <abhijeetsinha43@gmail.com>
2022-03-16 17:43:32 +0530

    feat: added index for images

---
+++ src/assets/index.js
@@ -0,0 +1,37 @@
+import productOne from "./product-1.jpg";
+import productTwo from "./product-2.jpg";
+import productThree from "./product-3.jpg";
+import productFour from "./product-4.jpg";
+import productFive from "./product-5.jpg";
+import productSix from "./product-6.jpg";
+import productSeven from "./product-7.jpg";
+import productEight from "./product-8.jpg";
+import productNine from "./product-9.jpg";
+import productTen from "./product-10.jpg";
+import productEleven from "./product-11.jpg";
+import productTwelve from "./product-12.jpg";
+import headerImg from "./image1.png";
+import categoryOne from "./category-1.jpg";
+import categoryTwo from "./category-2.jpg";
+import categoryThree from "./category-3.jpg";
+import galleryOne from "./gallery-1.jpg";
+
+export {
+  productOne,
+  productTwo,
+  productThree,
+  productFour,
+  productFive,
+  productSix,
+  productSeven,
+  productEight,
+  productNine,
+  productTen,
+  productEleven,
+  productTwelve,
+  headerImg,
+  categoryOne,
+  categoryTwo,
+  categoryThree,
+  galleryOne,
+};
