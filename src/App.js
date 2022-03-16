48625fbb1053ed5ff8af8784e77f98f14102924f
Abhijeet43 <abhijeetsinha43@gmail.com>
2022-03-16 21:24:02 +0530

    feat: added routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
+import React from "react";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import { Header, Footer } from "./components/index";
-import { Home } from "./pages/index";
+import {
+  Home,
+  Login,
+  Signup,
+  ProductListing,
+  ProductDetails,
+  Wishlist,
+  Cart,
+} from "./pages/index";
 
 function App() {
   return (
     <>
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/products" element={<ProductListing />} />
+        <Route path="/products/:productId" element={<ProductDetails />} />
+      </Routes>
       <Footer />
     </>
   );
