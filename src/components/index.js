48f8123f105cc97add0d1ca594c914fffae41a6c
Abhijeet43 <abhijeetsinha43@gmail.com>
2022-03-17 22:04:29 +0530

    refactor: moved filter,productPagination and CategoryCard to components and index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,6 @@
-export { Header } from './Header/Header';
-export { Footer } from './Footer/Footer';
-export { ProductCard } from './ProductCard/ProductCard';
+export { Header } from "./Header/Header";
+export { Footer } from "./Footer/Footer";
+export { Filters } from "./Filters/Filters";
+export { ProductCard } from "./ProductCard/ProductCard";
+export { ProductPagination } from "./ProductPagination/ProductPagination";
+export { CategoryCard } from "./CategoryCard/CategoryCard";
