8fca1a8b7ada1358c56313280c8d4e01ab895511
Abhijeet43 <abhijeetsinha43@gmail.com>
2022-03-17 22:10:51 +0530

    feat:added routes for categories and categorylisting

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter as Router } from "react-router-dom";
+import { DataProvider } from "./context/data-context";
 
 // Call make Server
 makeServer();
@@ -10,7 +11,9 @@ makeServer();
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <DataProvider>
+        <App />
+      </DataProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
