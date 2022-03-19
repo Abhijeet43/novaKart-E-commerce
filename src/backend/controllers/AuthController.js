f347f852cce568b54fd62564e594b3b4e869e8e8
Abhijeet43 <abhijeetsinha43@gmail.com>
2022-03-19 18:16:29 +0530

    fix: fixed auth controller file

--- src/backend/controllers/AuthController.js
+++ src/backend/controllers/AuthController.js
@@ -1,7 +1,9 @@
 import { v4 as uuid } from "uuid";
 import { Response } from "miragejs";
 import { formatDate } from "../utils/authUtils";
-const sign = require("jwt-encode");
+import bcrypt from "bcryptjs";
+const jwt = require("jsonwebtoken");
+
 /**
  * All the routes related to Auth are present here.
  * These are Publicly accessible routes.
@@ -13,7 +15,7 @@ const sign = require("jwt-encode");
  * body contains {firstName, lastName, email, password}
  * */
 
-export const signupHandler = function (schema, request) {
+export const signupHandler = function(schema, request) {
   const { email, password, ...rest } = JSON.parse(request.requestBody);
   try {
     // check if email already exists
@@ -28,10 +30,11 @@ export const signupHandler = function (schema, request) {
       );
     }
     const _id = uuid();
+    const encryptedPassword = bcrypt.hashSync(password, 5);
     const newUser = {
       _id,
       email,
-      password,
+      password: encryptedPassword,
       createdAt: formatDate(),
       updatedAt: formatDate(),
       ...rest,
@@ -39,7 +42,10 @@ export const signupHandler = function (schema, request) {
       wishlist: [],
     };
     const createdUser = schema.users.create(newUser);
-    const encodedToken = sign({ _id, email }, process.env.REACT_APP_JWT_SECRET);
+    const encodedToken = jwt.sign(
+      { _id, email },
+      process.env.REACT_APP_JWT_SECRET
+    );
     return new Response(201, {}, { createdUser, encodedToken });
   } catch (error) {
     return new Response(
@@ -58,7 +64,7 @@ export const signupHandler = function (schema, request) {
  * body contains {email, password}
  * */
 
-export const loginHandler = function (schema, request) {
+export const loginHandler = function(schema, request) {
   const { email, password } = JSON.parse(request.requestBody);
   try {
     const foundUser = schema.users.findBy({ email });
@@ -69,8 +75,8 @@ export const loginHandler = function (schema, request) {
         { errors: ["The email you entered is not Registered. Not Found error"] }
       );
     }
-    if (password === foundUser.password) {
-      const encodedToken = sign(
+    if (bcrypt.compareSync(password, foundUser.password)) {
+      const encodedToken = jwt.sign(
         { _id: foundUser._id, email },
         process.env.REACT_APP_JWT_SECRET
       );
