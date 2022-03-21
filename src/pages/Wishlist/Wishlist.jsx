66d6e1858d72d47839695bff0370af642f51a316
Abhijeet43 <abhijeetsinha43@gmail.com>
2022-03-21 10:54:29 +0530

    refactor: refactored wishlist page

--- src/pages/Wishlist/Wishlist.jsx
+++ src/pages/Wishlist/Wishlist.jsx
@@ -17,43 +17,7 @@ const Wishlist = () => {
           My Wishlist <span className="wishlist-items">(4)</span>
         </h2>
         <div className="card-container">
-          <ProductCard
-            image={productOne}
-            alt={"Red T-Shirt"}
-            title={"Red T-Shirt"}
-            badge={"New"}
-            price={"499"}
-            priceBefore={"1299"}
-            discount={"62"}
-          />
-
-          <ProductCard
-            image={productThree}
-            alt={"Gray Track"}
-            title={"Gray Track"}
-            badge={"New"}
-            price={"899"}
-            priceBefore={"1299"}
-            discount={"32"}
-          />
-          <ProductCard
-            image={productFour}
-            alt={"Blue T-Shirt"}
-            title={"Blue T-Shirt"}
-            badge={"New"}
-            price={"699"}
-            priceBefore={"899"}
-            discount={"42"}
-          />
-          <ProductCard
-            image={productFive}
-            alt={"White Shoes"}
-            title={"White Shoes"}
-            badge={"New"}
-            price={"499"}
-            priceBefore={"1299"}
-            discount={"62"}
-          />
+          <p>Wishlist is currently empty.</p>
         </div>
       </section>
     </main>
