db33cc9c5e74b213691e41b12d1ea30d73562d3b
Abhijeet43 <abhijeetsinha43@gmail.com>
2022-03-21 17:51:02 +0530

    update: updated wishlist icon

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -3,6 +3,7 @@ import "./ProductCard.css";
 import { Link, useNavigate } from "react-router-dom";
 import { addToCartHandler, checkAction } from "../../functions/";
 import { useAuth, useCart } from "../../context/";
+import { useToggle } from "../../hooks/useToggle";
 
 const ProductCard = ({ product }) => {
   const {
@@ -17,6 +18,7 @@ const ProductCard = ({ product }) => {
   } = product;
 
   const navigate = useNavigate();
+  const [wishlistToggle, setWishlistToggle] = useToggle(false);
   const size = sizes[0];
 
   const {
@@ -42,20 +44,10 @@ const ProductCard = ({ product }) => {
       </div>
       {badge ? <div className="card-badge">{badge}</div> : ""}
       <div className="card-icon">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          aria-hidden="true"
-          role="img"
-          className="iconify iconify--bi"
-          width="32"
-          height="32"
-          preserveAspectRatio="xMidYMid meet"
-          viewBox="0 0 16 16"
-        >
-          <g>
-            <path d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385c.92 1.815 2.834 3.989 6.286 6.357c3.452-2.368 5.365-4.542 6.286-6.357c.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"></path>
-          </g>
-        </svg>
+        <i
+          className={`${wishlistToggle ? "fa-solid" : "fa-regular"} fa-heart`}
+          onClick={setWishlistToggle}
+        ></i>
       </div>
       <div className="card-head">
         <h4 className="card-title">{title}</h4>
@@ -96,7 +88,12 @@ const ProductCard = ({ product }) => {
           className="card-btn card-btn-solid"
           onClick={() =>
             token
-              ? addToCartHandler(token, { ...product, size }, cartDispatch,cart)
+              ? addToCartHandler(
+                  token,
+                  { ...product, size },
+                  cartDispatch,
+                  cart
+                )
               : navigate("/login")
           }
         >
