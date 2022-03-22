70801ade50c07a56ed7cf97ed4e1cccfcc9b2daf
Abhijeet43 <abhijeetsinha43@gmail.com>
2022-03-22 17:31:47 +0530

    update: updated wishlist context

--- src/context/wishlist-context.js
+++ src/context/wishlist-context.js
@@ -1,14 +1,38 @@
 import React, { createContext, useContext, useReducer, useEffect } from "react";
 import { wishListReducer } from "../reducer/";
+import { useAuth } from "../context/index";
+import { loadWishlist } from "../functions/";
 
 const WishlistContext = createContext(null);
 
 const WishlistProvider = ({ children }) => {
+  const { token } = useAuth();
   const [wishlistState, wishlistDispatch] = useReducer(wishListReducer, {
     wishlist: [],
   });
+
+  useEffect(() => {
+    return token
+      ? (async () => {
+          try {
+            const response = await loadWishlist(token);
+            if (response.status === 201) {
+              wishlistDispatch({
+                type: "LOAD_WISHLIST",
+                payload: response.data.wishlist,
+              });
+            } else {
+              throw new Error("Something went wrong....Try Again");
+            }
+          } catch (error) {
+            console.log("Error", error);
+          }
+        })()
+      : null;
+  }, []);
+
   return (
-    <WishlistContext.Provider value={(wishlistState, wishlistDispatch)}>
+    <WishlistContext.Provider value={{ wishlistState, wishlistDispatch }}>
       {children}
     </WishlistContext.Provider>
   );
