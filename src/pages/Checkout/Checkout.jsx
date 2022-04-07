1f440c359c6473262762be375d70fe9f736dd5d1
Abhijeet43 <abhijeetsinha43@gmail.com>
2022-04-07 19:31:24 +0530

    refactor: removed unwanted code

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-// import Razorpay from "razorpay";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 import { useCart, useAddress, useAuth } from "../../context/";
