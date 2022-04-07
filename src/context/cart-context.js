59885f248b8b1acc1c18614d2d77b0c46b82765e
Abhijeet43 <abhijeetsinha43@gmail.com>
2022-04-07 22:30:45 +0530

    fix: fixes

--- src/context/cart-context.js
+++ src/context/cart-context.js
@@ -1,15 +1,9 @@
 import React, { createContext, useContext, useReducer } from "react";
 import { cartReducer } from "../reducer/";
-import { useAuth } from "./index";
-import { loadCart } from "../functions/";
 
 const CartContext = createContext(null);
 
 const CartProvider = ({ children }) => {
-  const {
-    authState: { token },
-  } = useAuth();
-
   const [cartState, cartDispatch] = useReducer(cartReducer, { cart: [] });
   return (
     <CartContext.Provider value={{ cartState, cartDispatch }}>
